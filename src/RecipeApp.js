4c817146d984c547f95261ef32de27c3740b6b8d
MansourMoukdad88 <m_sy@outlook.com>
2019-04-23 01:52:18 +0300

    Add state

--- src/RecipeApp.js
+++ src/RecipeApp.js
@@ -5,11 +5,50 @@ import Navbar from "./Navbar";
 import "./RecipeApp.css";
 
 class RecipeApp extends Component {
+  constructor(props) {
+    super(props);
+    this.state ={
+      recipes: [
+        {
+          id:1,
+          title: "Spaghetti",
+          instructions:
+            "Open jar of Spaghetti sauce.  Bring to simmer.  Boil water.  Cook pasta until done.  Combine pasta and sauce",
+          ingredients: ["pasta", "8 cups water", "1 box spaghetti"],
+          img: "http://recipetineats.com/wp-content/uploads/2014/06/Pasta1.jpg"
+        },
+        {
+          id:2,
+          title: "Milkshake",
+          instructions: "Combine ice cream and milk.  Blend until creamy",
+          ingredients: ["2 Scoops Ice cream", "8 ounces milk"],
+          img:
+            "https://www.wellplated.com/wp-content/uploads/2017/09/Healthy-Pumpkin-Milkshake.jpg"
+        },
+        {
+          id:3,
+          title: "Avocado Toast",
+          instructions:
+            "Toast bread.  Slice avocado and spread on bread.  Add salt, oil, and pepper to taste.",
+          ingredients: [
+            "2 slices of bread",
+            "1 avocado",
+            "1 tablespoon olive oil",
+            "1 pinch of salt",
+            "pepper"
+          ],
+          img:
+            "https://cookieandkate.com/images/2012/04/avocado-toast-variations.jpg"
+        }
+      ],
+      nextCreatedId:3,
+    }
+  }
   render() {
     return (
       <div className="App">
         <Navbar />
-        <RecipeList />
+        <RecipeList  recipes ={this.state.recipes}/>
       </div>
     );
   }
--- src/RecipeList.js
+++ src/RecipeList.js
@@ -4,44 +4,12 @@ import PropTypes from "prop-types";
 import "./RecipeList.css";
 
 class RecipeList extends Component {
-  static defaultProps = {
-    recipes: [
-      {
-        title: "Spaghetti",
-        instructions:
-          "Open jar of Spaghetti sauce.  Bring to simmer.  Boil water.  Cook pasta until done.  Combine pasta and sauce",
-        ingredients: ["pasta", "8 cups water", "1 box spaghetti"],
-        img: "http://recipetineats.com/wp-content/uploads/2014/06/Pasta1.jpg"
-      },
-      {
-        title: "Milkshake",
-        instructions: "Combine ice cream and milk.  Blend until creamy",
-        ingredients: ["2 Scoops Ice cream", "8 ounces milk"],
-        img:
-          "https://www.wellplated.com/wp-content/uploads/2017/09/Healthy-Pumpkin-Milkshake.jpg"
-      },
-      {
-        title: "Avocado Toast",
-        instructions:
-          "Toast bread.  Slice avocado and spread on bread.  Add salt, oil, and pepper to taste.",
-        ingredients: [
-          "2 slices of bread",
-          "1 avocado",
-          "1 tablespoon olive oil",
-          "1 pinch of salt",
-          "pepper"
-        ],
-        img:
-          "https://cookieandkate.com/images/2012/04/avocado-toast-variations.jpg"
-      }
-    ]
-  };
   static propsType = {
     recipes: PropTypes.arrayOf(PropTypes.object).isRequired
   };
   render() {
-    const recipes = this.props.recipes.map((r, index) => (
-      <Recipe ket={index} {...r} />
+    const recipes = this.props.recipes.map((r, id) => (
+      <Recipe key={r.id} {...r} />
     ));
     return <div className="recipe-list">{recipes}</div>;
   }
